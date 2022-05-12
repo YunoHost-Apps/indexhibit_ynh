95e87dcac39614282b0cf5dd54988d67ceb8c0e9
ericgaspar <junk.eg@free.fr>
2022-05-12 09:42:44 +0200

    First commit

---
+++ conf/config.example.php
@@ -0,0 +1,8 @@
+<?php  if (!defined('SITE')) exit('No direct script access allowed');
+
+$indx['db'] 		= '__DB_NAME__';
+$indx['user'] 		= '__DB_USER__';
+$indx['pass'] 		= '__DB_PWD__';
+$indx['host'] 		= 'localhost';
+$indx['sql']		= 'mysql';
+define('PX', 'ndxz_');
---
+++ doc/DISCLAIMER.md
@@ -0,0 +1,8 @@
+Post install:
+`www.yourdomain.com/ndxzstudio/install.php` to finish the install
+
+Database credentials are sent to the administrator's email address
+
+Admin area:
+`http://yourdomain.com/ndxz-studio` (where yourdomain.com is your domain)
+
